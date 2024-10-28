fbf7c263e404a31e8e76fc0e30dc5407b0d01416
anupam <anupamvns0099@gmail.com>
2024-10-28 04:58:22 +0000

    add Profile DocType

---
+++ solve_ninja/solve_ninja/doctype/profile/test_profile.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, ReapBenefit and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProfile(FrappeTestCase):
+	pass
