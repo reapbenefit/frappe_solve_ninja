38564e93503068747b1bf2364008d53bdac44216
anupam <anupamvns0099@gmail.com>
2024-11-13 09:19:01 +0000

    fix: report Users Action with No City

--- solve_ninja/solve_ninja/report/users_action_with_no_city/users_action_with_no_city.py
+++ solve_ninja/solve_ninja/report/users_action_with_no_city/users_action_with_no_city.py
@@ -2,7 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.query_builder.functions import Count
+from frappe.query_builder import functions as fn
 
 def execute(filters=None):
 	Events = frappe.qb.DocType("Events")
@@ -24,24 +24,32 @@ def execute(filters=None):
 			"width": 180,
 		}
 	]
+	# Subquery to find users with events that have locations with a city
+	users_with_city = (
+		frappe.qb.from_(User)
+		.join(Events).on(User.name == Events.user)
+		.join(Location).on(Events.location == Location.name)
+		.select(User.name)
+		.where(Location.city.isnotnull())  # Filters for events with a city in the location
+	)
+
+	# Main query to get users with events but exclude those with any location having a city
 	query = (
 		frappe.qb.from_(User)
-		.join(Events)
-		.on(User.name == Events.user)
-		.left_join(Location)
-		.on(Events.location == Location.name)
+		.join(Events).on(User.name == Events.user)
+		.left_join(Location).on(Events.location == Location.name)
 		.select(
-			Count(Events.name).as_("actions"),
+			fn.Count(Events.name).as_("actions"),
 			User.name.as_("user")
 		)
 		.where(
-			Location.city.isnull()
+			User.name.notin(users_with_city)  # Excludes users who have events with locations that have a city
 		)
-		.groupby(
-			User.name
-		).orderby(
-			Count(Events.name), order=frappe.qb.desc
+		.groupby(User.name)
+		.having(
+			fn.Count(Events.name) > 0  # Ensures the user has at least one event
 		)
+		.orderby(fn.Count(Events.name), order=frappe.qb.desc)
 	)
 
 	# Run the query with debug enabled
