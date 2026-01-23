173df18b60bec397d9d52f17174681821ecc9407
Ankit Saxena <ankit.saxena@reapbenefit.org>
2026-01-23 10:45:37 +0000

    release: Week 4 2026

---
+++ solve_ninja/solve_ninja/doctype/mentee_feedback/mentee_feedback.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, ReapBenefit and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Mentee Feedback', {
+	// refresh: function(frm) {
+
+	// }
+});
